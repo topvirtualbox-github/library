08368e72edb0e643614a8ccd8e8f255a183e9aaa
topvirtualbox <topvirtualbox@gmail.com>
2021-08-10 19:06:27 -0300

    Add local storage

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-let myLibrary = [
+const defaultLibrary = [
     book1 = { title: "1984", author: "George Orwell", pages: "328", read: "YES" },
     book2 = { title: "Fahrenheit 451", author: "Ray Bradbury", pages: "256", read: "NO" },
     book3 = { title: "Animal Farm", author: "George Orwell", pages: "112", read: "YES" }
 ];
 
-displayBooks();
-
-const addButton = document.querySelector(".button-add");
-const submitButton = document.querySelector(".button-submit");
-const closeButton = document.querySelector(".button-close");
-const backgroundForm = document.querySelector(".form-background");
-
-addButton.addEventListener("click", () => {
-    backgroundForm.classList.add("form-background-active");
-});
-submitButton.addEventListener("click", addBook);
-closeButton.addEventListener("click", () => {
-    backgroundForm.classList.remove("form-background-active");
-});
-backgroundForm.addEventListener("click", (e) => {
-    if (e.target === backgroundForm) { backgroundForm.classList.remove("form-background-active"); }
-});
+let customLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -37,7 +21,10 @@ function addBook(e) {
     if (titleValue === "" || authorValue === "" || pagesValue === "") return;
     e.preventDefault();
     const newBook = new Book(titleValue, authorValue, pagesValue, readValue);
-    myLibrary.push(newBook);
+    customLibrary.push(newBook);
+
+    localStorage.setItem("customLibrary", JSON.stringify(customLibrary));
+
     document.querySelector("form").reset();
     displayBooks();
 }
@@ -47,9 +34,9 @@ function displayBooks() {
     while (display.hasChildNodes()) {
         display.removeChild(display.lastChild);
     }
-    myLibrary.forEach(book => {
+    customLibrary.forEach(book => {
         const newBook = document.createElement("div");
-        newBook.dataset.id = myLibrary.indexOf(book);
+        newBook.dataset.id = customLibrary.indexOf(book);
         newBook.className = "item";
         display.appendChild(newBook);
         const newTitle = document.createElement("div");
@@ -67,8 +54,8 @@ function displayBooks() {
         else { newReadButton.textContent = "NO"; }
         newBook.appendChild(newReadButton);
         newReadButton.addEventListener("click", () => {
-            if (myLibrary[newBook.dataset.id].read === "YES") { myLibrary[newBook.dataset.id].read = "NO"; }
-            else { myLibrary[newBook.dataset.id].read = "YES"; }
+            if (customLibrary[newBook.dataset.id].read === "YES") { customLibrary[newBook.dataset.id].read = "NO"; }
+            else { customLibrary[newBook.dataset.id].read = "YES"; }
             displayBooks();
         });
         const newDeleteButton = document.createElement("button");
@@ -76,8 +63,41 @@ function displayBooks() {
         newDeleteButton.textContent = "X";
         newBook.appendChild(newDeleteButton);
         newDeleteButton.addEventListener("click", () => {
-            myLibrary.splice(newBook.dataset.id, 1);
+            customLibrary.splice(newBook.dataset.id, 1);
+            saveLocalStorage();
             displayBooks();
         });
     });
 }
+
+function saveLocalStorage() {
+    localStorage.setItem("customLibrary", JSON.stringify(customLibrary));
+}
+
+function getLocalStorage() {
+    const storage = localStorage.getItem("customLibrary");
+    if (!storage || storage === "[]") {
+        customLibrary = defaultLibrary;
+    } else {
+        customLibrary = JSON.parse(localStorage.getItem("customLibrary"));
+    }
+}
+
+getLocalStorage();
+displayBooks();
+
+const addButton = document.querySelector(".button-add");
+const submitButton = document.querySelector(".button-submit");
+const closeButton = document.querySelector(".button-close");
+const backgroundForm = document.querySelector(".form-background");
+
+addButton.addEventListener("click", () => {
+    backgroundForm.classList.add("form-background-active");
+});
+submitButton.addEventListener("click", addBook);
+closeButton.addEventListener("click", () => {
+    backgroundForm.classList.remove("form-background-active");
+});
+backgroundForm.addEventListener("click", (e) => {
+    if (e.target === backgroundForm) { backgroundForm.classList.remove("form-background-active"); }
+});
