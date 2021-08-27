af7571b29e4522b5fd0b4107b9c0768a5c08c1e1
topvirtualbox <topvirtualbox@gmail.com>
2021-08-27 17:41:53 -0300

    Replace constructor function with a class

--- script.js
+++ script.js
@@ -8,11 +8,20 @@ const defaultLibrary = [
 
 let customLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBook(e) {
